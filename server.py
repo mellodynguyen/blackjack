d26c3e93016ed1347ca4ef9ea1e1605df749634e
Mellody <mellodynguyen@outlook.com>
2024-01-17 08:13:07 -0600

    initial commit with finished terminal game

---
+++ server.py
@@ -0,0 +1,179 @@
+import random
+
+print("⠀⠀⠀⠀⠀⠀⠀⢀⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣤⣤⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀")
+print("⠀⠀⠀⠀⠀⡀⣰⡿⠛⠛⠿⢶⣦⣀⠀⢀⣀⣀⣀⣀⣠⡾⠋⠀⠀⠹⣷⣄⣤⣶⡶⠿⠿⣷⡄⠀⠀⠀")
+print("⠀⠀⠀⠀⠀⢰⣿⠁⠀⠀⠀⠀⠈⠙⠛⠛⠋⠉⠉⢹⡟⠁⠀⠀⣀⣀⠘⣿⠉⠀⠀⠀⠀⠘⣿⠀⠀⠀⠀⠀")
+print("⠀⠀⠀⠀⠀⢸⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⠁⠀⠀⣾⡋⣽⠿⠛⠿⢶⣤⣤⣤⣤⣿⠀⠀⠀⠀⠀")
+print(" ⠀⠀⠀⠀⠀⢸⣿⡴⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢻⣄⡀⠀⢈⣻⡏⠀⠀⠀⠀⣿⣀⠀⠈⠙⣷⠀⠀⠀⠀")
+print(" ⠀⠀⠀⠀⠀⣰⡿⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⠛⠛⠛⠙⢷⣄⣀⣀⣼⣏⣿⠀⠀⢀⣿⠀⠀⠀⠀")
+print(" ⠀⠀⠀⠀⢸⡟⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠙⣿⡉⠉⠁⢀⣠⣿⡇⠀⠀⠀⠀")
+print(" ⠀⠀⠀⠀⣿⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠛⠗⠾⠟⠋⢹⣷⠀⠀⠀⠀")
+print(" ⢀⣤⣤⣤⣿⣤⣄⠀⠀⠀⠴⠚⠲⠄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣶⡆⠀⠀⠀⠀⢀⣈⣿⣀⣀⡀⠀")
+print(" ⠀⠀⠀⠈⣿⣠⣾⠟⠛⢷⡄⠀⠀⠀⠀⠀⠀⠀⡤⠶⢦⡀⠀⠀⠀⠀⠹⠯⠃⠀⠀⠀⠈⠉⢩⡿⠉⠉⠉⠁")
+print(" ⠀⠀⣤⡶⠿⣿⣇⠀⠀⠸⣷⠀⠀⠀⠀⠀⠀⠀⠓⠶⠞⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢤⣼⣯⣀⣀⠀⠀")
+print(" ⠀⢰⣯⠀⠀⠈⠻⠀⠀⠀⣿⣶⣤⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣰⡿⠁⠉⠉⠁⠀")
+print(" ⠀⠀⠙⣷⣄⠀⠀⠀⠀⠀⢀⣀⣀⠙⢿⣆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢈⣿⡿⢷⣄⡀⠀⠀⠀")
+print(" ⠀⠀⠀⠈⠙⣷⠀⠀⠀⣴⠟⠉⠉⠀⠀⣿⣀⣀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣀⣠⣤⣾⠟⠉⠀⠀⠈⠉⠀⠀⠀")
+print(" ⠀⠀⠀⠀⠰⣿⠀⠀⠀⠙⢧⣤⡶⠟⢀⣿⠛⢟⡟⡯⠽⢶⡶⠾⢿⣻⣏⣹⡏⣁⡿⠀⠀⠀⠀⠀⠀⠀⠀⠀")
+print(" ⠀⠀⠀⠀⠀⠹⣷⣄⠀⠀⠀⠀⠀⣠⣾⠏⠀⠀⠙⠛⠛⠋⠀⠀⢀⣽⠟⠛⠖⠛⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀")
+print(" ⠀⠀⠀⠀⠀⠀⠀⠙⠻⠷⠶⠿⠟⠋⠹⣷⣤⣀⡀⠄⣡⣀⣠⣴⡿⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀")
+print(" ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠳⣍⣉⣻⣏⣉⣡⠞⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀")
+print(" ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀")
+print("Welcome to ♡𝓗𝓮𝓵𝓵𝓸 𝓴𝓲𝓽𝓽𝔂's♡ Blackjack Game!!")
+print()
+
+
+def play_game(): 
+  """our main function that will run the entire game"""
+  def rules(): 
+    """shows the user the rules of the game"""
+    print("RULES ~")
+    print()
+    print("Rule 1:")
+    print("In blackjack, each player and dealer receives two cards!")
+    print()
+    print("Rule 2:")
+    print("The player (you) can then choose to “hit” (request another card) or “stand”")
+    print("The goal in this game is to keep the sum of all card points equal to 21 without exceeding it or bust!")
+    print()
+    print("Rule 3:")
+    print("Face cards (i.e. Jack, Queen, and King) are worth 10 points, and Aces can be worth 11 points.")
+    print("If a player has more than 21 points then the player automatically loses the game.")
+    print()
+  
+  rules()
+  
+  # 52 cards - [hearts, diamonds, clubs, spades]  [all the numbers & faces]
+  
+  card_suits = ["hearts", "diamonds", "clubs", "spades"]
+  cards_list = ["Ace","1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen","King"]
+  
+  deck = [(card, suit) for suit in card_suits for card in cards_list]
+  
+  # numbered cards can be equal to the value they're assigned
+  # face cards needs to be = 10 and ace needs to be 11
+  
+  def card_values(card):
+    """adds value to the face cards and ace, keeps numbered cards = to the value they're assigned to"""
+    if card[0] in ['Jack', 'Queen', 'King']:
+      return 10
+    elif card[0] == 'Ace':
+      return 11
+    else:
+      return int(card[0])
+  
+  # need to write a code that deals a hand for the player and the dealer/house 
+  # shuffles the deck - import random
+  
+  random.shuffle(deck)
+  player_card = [deck.pop(), deck.pop()]
+  dealer_card = [deck.pop(), deck.pop()]
+  # .pop to remove the cards that have been played
+
+  player_answer = input("Would you like to play? [yes or no] > ")
+  
+  player_answer = player_answer.lower()
+
+  
+  # def playing_hand
+  while player_answer == "yes": 
+     # deal cards
+    player_score = sum(card_values(card) for card in player_card)
+    dealer_score = sum(card_values(card) for card in dealer_card)
+      
+    # shows/displays the user's current hand 
+    print("Your cards:", player_card)
+    print("Your score is", player_score)
+    print("")
+      
+     # get input from user whether to stand or hit
+    choice = input('Do you want to hit or stand? ["hit" to request another card, "stand" to stand]: ').lower()
+    if choice == "hit":
+      new_card = deck.pop()
+      player_card.append(new_card)
+      # dealer_card.append(new_card)
+      player_score = sum(card_values(card) for card in player_card)
+      dealer_score = sum(card_values(card) for card in dealer_card)
+
+    
+      # while dealer_score < 17:
+      #   new_card = deck.pop()
+      #   dealer_card.append(new_card)
+      #   dealer_score += card_values(new_card)
+      # technically the dealer is supposed to hit at 16
+    elif choice == "stand":
+      break
+    else:
+      print("Invalid choice. Please try again.")
+      continue
+      # shows/displays the user's current hand
+    print("Your cards:", player_card)
+    print("Your score is", player_score)
+    print("")
+
+    print("The Dealer Has:", dealer_card)
+    print("Score Of The Dealer:", dealer_score)
+    print("")
+
+  while player_answer == "yes":
+    if player_score > 21:
+      print("Dealer Has:", dealer_card)
+      print("Score Of The Dealer:", dealer_score)
+      print("You have:", player_card)
+      print("Score Of The Player:", player_score)
+      print("Bust! You lose :(")
+      break
+    elif dealer_score > 21:
+      print("Dealer Has:", dealer_card)
+      print("Score Of The Dealer:", dealer_score)
+      print("You have:", player_card)
+      print("Score Of The Player:", player_score)
+      print("Dealer Busts!")
+      print("")
+      break
+    elif player_score > dealer_score:
+      print("Dealer Has:", dealer_card)
+      print("Score Of The Dealer:", dealer_score)
+      print("You have:", player_card)
+      print("Score Of The Player:", player_score)
+      print("")
+      print("Player wins!")
+      print("")
+      print("⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀  ⣠⣤⣄⠀⠀⠀⠀⠀⠀⠀")
+      print("⠀⠀⠀⣰⠞⠛⠲⣦⣤⠤⠤⢴⠏⣦⣈⣷⣚⠉⠻⡄⠀⠀")
+      print("⠀⠀⠀⣿⣀⠀⠈⠁⠀⠀⠀⢾⡀⢻⡿⠉⠉⣷⡞⠻⣦⠀")
+      print("⠀⠀⢀⡿⠁⠀⠀⠀⠀⠀⠀⠈⠛⠛⠷⣤⣴⠿⠿⣠⡟⠀")
+      print("⠀⠀⣾⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠻⢦⡶⢻⡄⠀")
+      print("⠠⠤⡧⠄⠀⣤⣄⠀⠀⠀⠀⠀⠀⠀⠀⣤⣄⠀⠈⢹⡏⠁")
+      print("⠀⢀⣿⡤⠀⢻⡏⠀⠀⠀⣞⣻⠀⠀⠀⠻⠏⠀⡈⣽⠛⠃")
+      print("⠀⠀⢈⡷⣶⣃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣠⠿⠛⠂⠀")
+      print("⠀⠀⠀⠀⠀⠉⠙⠛⠛⠓⠒⠒⠒⠚⠛⠛⠉⠁⠀")
+      print("")
+      break
+    elif dealer_score > player_score:
+      print("Dealer Has:", dealer_card)
+      print("Score Of The Dealer:", dealer_score)
+      print("You have:", player_card)
+      print("Score Of The Player:", player_score)
+      print("")
+      print("Dealer wins :(")
+      print("")
+      break
+      # tie
+    elif dealer_score == player_score:
+      print("Dealer Has:", dealer_card)
+      print("Score Of The Dealer:", dealer_score)
+      print("You have:", player_card)
+      print("Score Of The Player:", player_score)
+      print("")
+      print("It's a tie!")
+      print("")
+      break
+
+  # prompts user if they want to play again
+  again = input("Do you want to play again? > ")
+  if again.startswith("y") or again.startswith("Y"):
+    play_game()
+
+# call our main function 
+play_game()
